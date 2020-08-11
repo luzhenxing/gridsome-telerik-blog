4170b85e0bd8329fb7bf5b08f5ecc1408dcd7949
luzhenxing <luzhenxing_1230@163.com>
2020-08-11 09:42:30 +0800

    commit code

---
+++ gridsome.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  siteName: 'Gridsome Blog',
+  siteDescription: 'A simple blog designed with Gridsome',
+  templates: {
+    Post: '/blog/:title',
+  },
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'content/posts/**/*.md',
+        typeName: 'Post',
+      }
+    }
+  ],
+  transformers: {
+    //Add markdown support to all file-system sources
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      plugins: [
+        '@gridsome/remark-prismjs'
+      ]
+    }
+  },
+}
---
+++ src/pages/Index.vue
@@ -0,0 +1,42 @@
+<template>
+  <Layout>
+    <header class="header">
+      <h1>Gridsome Blog</h1>
+      <p>A simple blog designed with Gridsome</p>
+    </header>
+    <section class="posts">
+      <PostList v-for="edge in $page.allPost.edges" :key="edge.node.id" :post="edge.node" />
+    </section>
+  </Layout>
+</template>
+<script>
+import PostList from "@/components/PostList";
+export default {
+  components: {
+    PostList
+  }
+};
+</script>
+<page-query>
+query {
+  allPost {
+    totalCount
+    edges {
+      node {
+        id
+        title
+        timeToRead
+        description
+        date (format: "D MMMM YYYY")
+        path
+      }
+    }
+  }
+}
+</page-query>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
